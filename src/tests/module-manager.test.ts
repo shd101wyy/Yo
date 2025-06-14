d02e0af2fe1ae7582881dfa83bc90f6703ff4ce0
Yiyi Wang <shd101wyy@gmail.com>
2025-06-14 20:18:36 +0800

    chore: Start migrating old example code files

--- src/tests/module-manager.test.ts
+++ src/tests/module-manager.test.ts
@@ -1,17 +1,17 @@
-import * as fs from "fs";
+import { globSync } from "glob";
 import * as path from "path";
 import { ModuleManager } from "../module-manager";
 
 describe("Module Manager Tests", () => {
   const examplesDir = path.join(__dirname, "examples");
-  const files = fs.readdirSync(examplesDir).filter((f) => f.endsWith(".yo"));
+  const files = globSync(`${examplesDir}/**/*.yo`, {
+    ignore: [`${examplesDir}/not_working_yet/**`],
+  });
 
   files.forEach((file) => {
     it(`should load and evaluate ${file} correctly`, () => {
       const moduleManager = new ModuleManager();
-      const { moduleError } = moduleManager.loadModule(
-        "file://" + path.join(examplesDir, file)
-      );
+      const { moduleError } = moduleManager.loadModule("file://" + file);
       if (moduleError) {
         throw moduleError;
       }
