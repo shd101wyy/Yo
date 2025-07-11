753e1b434bc03d07311d699f0e03d185dbcfd7cc
Yiyi Wang <shd101wyy@gmail.com>
2025-07-11 11:18:06 +0800

    feat: Extend the for to support index

--- src/evaluator/exprs/for.ts
+++ src/evaluator/exprs/for.ts
@@ -11,14 +11,26 @@ import {
   BuiltinKeywords,
   expectExprToBeFunctionCallOf,
   Expr,
+  exprIsAtom,
   exprIsFunctionCall,
+  exprIsFunctionCallOf,
   exprToString,
   FuncCallExpr,
   setExprAsConsumed,
 } from "../../expr";
-import { isArrayType, isUnitType, typeToString } from "../../types";
+import {
+  createUsizeType,
+  isArrayType,
+  isUnitType,
+  typeToString,
+} from "../../types";
 import { VUnit } from "../../unit-value";
-import { isArrayValue } from "../../value";
+import {
+  createNumberValue,
+  createUnknownValue,
+  isArrayValue,
+} from "../../value";
+import { ValueTag } from "../../value-tag";
 import { EvaluatorContext } from "../context";
 import { isValidVariableName } from "../utils";
 import { evaluateBeginExpression } from "./begin";
@@ -27,6 +39,8 @@ import { evaluateBeginExpression } from "./begin";
  * For loop
  *
  * for <Array value>, <element value> => body
+ * for <Array value>, (<element value>,) => body
+ * for <Array value>, (<element value>, <element index>) => body
  */
 export function evaluateFor({
   expr,
@@ -41,15 +55,51 @@ export function evaluateFor({
   expectExprToBeFunctionCallOf(expr.args[1]!, "=>", 2);
 
   const arrayValueExpr: Expr = expr.args[0]!;
-  const bindingExpr: Expr = expr.args[1]!;
-  if (!exprIsFunctionCall(bindingExpr)) {
+  const arrowExpr: Expr = expr.args[1]!;
+  if (!exprIsFunctionCall(arrowExpr)) {
     throw formatErrorMessage({
-      token: bindingExpr.token,
-      errorMessage: `Expected the second argument of 'for' to be =>, got:\n${exprToString(bindingExpr)}`,
+      token: arrowExpr.token,
+      errorMessage: `Expected the second argument of 'for' to be =>, got:\n${exprToString(arrowExpr)}`,
     });
   }
-  const elementVariableExpr: Expr = bindingExpr.args[0]!;
-  const bodyExpr: Expr = bindingExpr.args[1]!;
+  const bindingExpr: Expr = arrowExpr.args[0]!;
+  const bodyExpr: Expr = arrowExpr.args[1]!;
+
+  let elementVariableExpr: Expr | undefined;
+  let elementIndexExpr: Expr | undefined;
+  let isElementVariableMutable = false;
+  if (exprIsAtom(bindingExpr)) {
+    elementVariableExpr = bindingExpr;
+  } else if (
+    exprIsFunctionCall(bindingExpr) &&
+    exprIsFunctionCallOf(bindingExpr, BuiltinKeywords.mut)
+  ) {
+    isElementVariableMutable = true;
+    elementVariableExpr = bindingExpr.args[0];
+  } else {
+    if (!exprIsFunctionCallOf(bindingExpr, BuiltinKeywords.tuple)) {
+      throw formatErrorMessage({
+        token: bindingExpr.token,
+        errorMessage: `Expected the binding expression to be a variable or a tuple, got:\n${exprToString(bindingExpr)}`,
+      });
+    }
+    elementVariableExpr = bindingExpr.args[0];
+    elementIndexExpr = bindingExpr.args[1];
+
+    if (
+      exprIsFunctionCall(elementVariableExpr) &&
+      exprIsFunctionCallOf(elementVariableExpr, BuiltinKeywords.mut)
+    ) {
+      isElementVariableMutable = true;
+      elementVariableExpr = elementVariableExpr.args[0];
+    }
+  }
+  if (!elementVariableExpr) {
+    throw formatErrorMessage({
+      token: bindingExpr.token,
+      errorMessage: `Expected the binding expression to have at least one element, got:\n${exprToString(bindingExpr)}`,
+    });
+  }
 
   // Expect the elementValueExpr to be a variable
   if (!isValidVariableName(elementVariableExpr)) {
@@ -60,7 +110,15 @@ export function evaluateFor({
       )}`,
     });
   }
-  const elementVariableName = elementVariableExpr.token.value;
+  // Expect the elementIndexExpr to be a variable or undefined
+  if (elementIndexExpr && !isValidVariableName(elementIndexExpr)) {
+    throw formatErrorMessage({
+      token: elementIndexExpr.token,
+      errorMessage: `Invalid variable name for element index, got:\n${exprToString(
+        elementIndexExpr
+      )}`,
+    });
+  }
 
   // Evaluate the arrayValue expression
   const evaluatedArrayValueExpr = context.evaluateExpression({
@@ -91,14 +149,18 @@ export function evaluateFor({
   // Set the array value as consumed
   setExprAsConsumed(evaluatedArrayValueExpr, env, context);
 
-  // Add the elementVariable to the environment
+  // Add the elementVariable and index to the environment
+  const elementVariableName = elementVariableExpr.token.value;
+  const elementIndexName = elementIndexExpr
+    ? elementIndexExpr.token.value
+    : undefined;
   env = pushEnvFrame(env);
   const { env: nextEnv } = addVariableToEnv({
     env,
     variable: {
       name: elementVariableName,
       type: arrayType.elementType,
-      isMutable: true, // The element variable is mutable
+      isMutable: isElementVariableMutable,
       consumedAtToken: undefined,
       initializedAtToken: elementVariableExpr.token,
       isCompileTimeOnly: Boolean(evaluatedArrayValueExpr.$.value),
@@ -108,6 +170,46 @@ export function evaluateFor({
     },
   });
   env = nextEnv;
+  /// Add type information to the element variable expr
+  elementVariableExpr.$ = {
+    type: arrayType.elementType,
+    isMutable: isElementVariableMutable,
+    env,
+    value: evaluatedArrayValueExpr.$.value
+      ? createUnknownValue(arrayType.elementType, elementVariableName)
+      : undefined,
+    pathCollection: [],
+  };
+
+  if (elementIndexName) {
+    // Add the element index variable to the environment
+    const { env: nextEnv } = addVariableToEnv({
+      env,
+      variable: {
+        name: elementIndexName,
+        type: createUsizeType(),
+        isMutable: false, // The index variable is immutable
+        consumedAtToken: undefined,
+        initializedAtToken: elementIndexExpr!.token,
+        isCompileTimeOnly: Boolean(evaluatedArrayValueExpr.$.value), // Not a compile-time only variable
+        isImplicit: false, // Not an implicit variable
+        token: elementIndexExpr!.token,
+        value: undefined, // Let's set it as undefined for now, then we initialize it in the for loop body
+      },
+    });
+    env = nextEnv;
+
+    /// Add type information to the element index variable expr
+    elementIndexExpr!.$ = {
+      type: createUsizeType(),
+      isMutable: false, // The index variable is immutable
+      env,
+      value: evaluatedArrayValueExpr.$.value
+        ? createUnknownValue(createUsizeType(), elementIndexName) // Initialize it to 0
+        : undefined,
+      pathCollection: [],
+    };
+  }
 
   // Get the array value
   const arrayValue = evaluatedArrayValueExpr.$.value;
@@ -130,6 +232,18 @@ export function evaluateFor({
       consumedAtToken: undefined, // Reset consumedAtToken
       value: isArrayValue(arrayValue) ? arrayValue.elements[index]! : undefined, // Set the value to the current element
     });
+
+    // Update the element index variable in the environment if it exists
+    if (elementIndexName) {
+      const elementIndexVariables = getVariablesFromEnv(env, elementIndexName);
+      const elementIndexVariable =
+        elementIndexVariables[elementIndexVariables.length - 1]!;
+      env = updateExistingVariable(env, elementIndexVariable, {
+        ...elementIndexVariable,
+        consumedAtToken: undefined, // Reset consumedAtToken
+        value: createNumberValue(ValueTag.Usize, index), // Set the value to the current index
+      });
+    }
 
     // Evaluate the body
     const evaluatedBodyExpr = evaluateBeginExpression({
