8574ff1132c7fe9ec509718099adef7e4c47c995
Yiyi Wang <shd101wyy@gmail.com>
2025-10-27 10:39:52 +0800

    feat: Support ':' for export renaming

--- src/evaluator/values/anonymous_module.ts
+++ src/evaluator/values/anonymous_module.ts
@@ -236,41 +236,87 @@ export function evaluateAnonymousModuleBeginExprs({
                 };
               }
             }
-          } else {
-            if (!isValidVariableName(exportExpr)) {
-              throw formatErrorMessage({
-                token: exportExpr.token,
-                errorMessage: `Expected identifier for export, got:\n${exprToString(exportExpr)}`,
-              });
+          }
+          // Check if it's
+          //          variableName        givenVariableName
+          // - export Add
+          // - export Add           :     _Add
+          else {
+            let variableName: string = "";
+            let givenVariableName: string = "";
+            if (exprIsAtom(exportExpr)) {
+              if (!isValidVariableName(exportExpr)) {
+                throw formatErrorMessage({
+                  token: exportExpr.token,
+                  errorMessage: `Expected identifier for export, got:\n${exprToString(exportExpr)}`,
+                });
+              }
+
+              variableName = exportExpr.token.value;
+              givenVariableName = variableName;
+            } else if (
+              exprIsFunctionCall(exportExpr) &&
+              exprIsFunctionCallOf(exportExpr, ":", 2)
+            ) {
+              // Check if it's export Add : _Add
+              const labelExpr = exportExpr.args[0]!;
+              const nameExpr = exportExpr.args[1]!;
+
+              if (!exprIsAtom(labelExpr)) {
+                throw formatErrorMessage({
+                  token: labelExpr.token,
+                  errorMessage: `Expected identifier for export, got:\n${exprToString(labelExpr)}`,
+                });
+              }
+              if (!isValidVariableName(labelExpr)) {
+                throw formatErrorMessage({
+                  token: labelExpr.token,
+                  errorMessage: `Expected identifier for export, got:\n${exprToString(labelExpr)}`,
+                });
+              }
+              variableName = labelExpr.token.value;
+
+              if (!exprIsAtom(nameExpr)) {
+                throw formatErrorMessage({
+                  token: nameExpr.token,
+                  errorMessage: `Expected identifier for export, got:\n${exprToString(nameExpr)}`,
+                });
+              }
+              if (!isValidVariableName(nameExpr)) {
+                throw formatErrorMessage({
+                  token: nameExpr.token,
+                  errorMessage: `Expected identifier for export, got:\n${exprToString(nameExpr)}`,
+                });
+              }
+              givenVariableName = nameExpr.token.value;
             }
 
-            const variableName = exportExpr.token.value;
             // Get the variable from the env
-            const variables = getVariablesFromEnv(env, variableName);
+            const variables = getVariablesFromEnv(env, givenVariableName);
             if (variables.length === 0) {
               throw formatErrorMessage({
                 token: exportExpr.token,
-                errorMessage: `Variable "${variableName}" is not defined in the module.`,
+                errorMessage: `Variable "${givenVariableName}" is not defined in the module.`,
               });
             }
             const variable = variables[variables.length - 1]!;
 
             // Check if the same variable is already exported
             const existingElementIndex = moduleType.elements.findIndex(
-              (e) => e.label === variableName
+              (e) => e.label === givenVariableName
             );
             if (existingElementIndex >= 0) {
               // Throw error if the variable is already exported
               throw formatErrorMessage({
                 token: exportExpr.token,
-                errorMessage: `Variable "${variableName}" is already exported in the module.`,
+                errorMessage: `Variable "${givenVariableName}" is already exported in the module.`,
               });
             } else {
               // Prevent exporting runtime variable
               if (!variable.isCompileTimeOnly) {
                 throw formatErrorMessage({
                   token: exportExpr.token,
-                  errorMessage: `Variable "${variableName}" is not a compile-time variable and cannot be exported.`,
+                  errorMessage: `Variable "${givenVariableName}" is not a compile-time variable and cannot be exported.`,
                 });
               }
 
