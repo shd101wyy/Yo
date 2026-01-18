ae673249755f84e931b9fbf5b2681714c0e062df
Yiyi Wang <shd101wyy@gmail.com>
2026-01-18 11:04:19 +0800

    Revert "refactor: Refactor the C codegen expr generation"
    This reverts commit a559e896b042f17bce24e773e5e3177869b7a88b.

--- src/codegen/expressions/index.ts
+++ src/codegen/expressions/index.ts
@@ -1,6 +1,1 @@
-export { generateAsyncBlock } from "./async-block";
-export {
-  generateDeferredDropExpressions,
-  generateDeferredDupExpressions,
-} from "./control-flow";
 export * from "./generation";
